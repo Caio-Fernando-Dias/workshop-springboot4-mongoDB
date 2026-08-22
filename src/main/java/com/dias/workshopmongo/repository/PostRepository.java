3ab0ccf716e4bed14ae4d6b5f979d487c7fa7883
Caio Fernando Dias <caio.dias@sou.unifal-mg.edu.br>
2026-08-22 16:06:30 -0300

    Consulta com varios criterios

--- src/main/java/com/dias/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/dias/workshopmongo/repository/PostRepository.java
@@ -16,4 +16,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
+    @Query("{ $and: [ {date: {$get: ?1} }, {$lte: ?2}, {$or: [ { 'title': { ${regex: ?0, $options: 'i' } }, { 'body': { ${regex: ?0, $options: 'i' } }, { 'comments.text': { ${regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, java.util.Date min, java.util.Date max);
+
 }
