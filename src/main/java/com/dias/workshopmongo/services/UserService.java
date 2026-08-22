f8320c781fce1014e7e074f0cbc18f52980426b5
Caio Fernando Dias <caio.dias@sou.unifal-mg.edu.br>
2026-08-22 13:38:07 -0300

    Insercao de usuario com POST

--- src/main/java/com/dias/workshopmongo/services/UserService.java
+++ src/main/java/com/dias/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dias.workshopmongo.domain.User;
+import com.dias.workshopmongo.dto.UserDTO;
 import com.dias.workshopmongo.repository.UserRepository;
 import com.dias.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = repo.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
